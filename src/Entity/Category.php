cf136d447aa305124dfa590ca455f20bf1307f65
sergej.moskeet <sergej.moskeet@gmail.com>
2018-12-21 17:04:06 +0200

    entities + graphql started

---
+++ src/Entity/Category.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\IdTrait;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table
+ */
+class Category
+{
+    use IdTrait;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=250)
+     */
+    private $name;
+
+    /**
+     * @var Item[]
+     *
+     * @ORM\OneToMany(
+     *     targetEntity="App\Entity\Category",
+     *     mappedBy="category"
+     * )
+     */
+    private $items;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $value
+     * @return $this
+     */
+    public function setName(string $value): self
+    {
+        $this->name = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return Item[]
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param Item[] $value
+     * @return $this
+     */
+    public function setItems(array $value): self
+    {
+        $this->items = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param Item $value
+     * @return $this
+     */
+    public function addItem($value): self
+    {
+        $this->items[] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param Item $value
+     * @return $this
+     */
+    public function removeItem($value): self
+    {
+        if (($key = array_search($value, $this->items, true)) !== false) {
+            array_splice($this->items, $key, 1);
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Item.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\IdTrait;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table
+ */
+class Item
+{
+    use IdTrait;
+
+    /**
+     * @var Category
+     *
+     * @ORM\ManyToOne(
+     *     targetEntity="App\Entity\Category",
+     *     inversedBy="items"
+     * )
+     */
+    private $category;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=250)
+     */
+    private $name;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(type="decimal", precision=2, scale=12)
+     */
+    private $price;
+
+    /**
+     * @return Category
+     */
+    public function getCategory(): Category
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param Category $value
+     * @return $this
+     */
+    public function setCategory(Category $value): self
+    {
+        $this->category = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $value
+     * @return $this
+     */
+    public function setName(string $value): self
+    {
+        $this->name = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $value
+     * @return $this
+     */
+    public function setPrice(float $value): self
+    {
+        $this->price = $value;
+
+        return $this;
+    }
+}
